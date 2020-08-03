cc8fbb30985588c5629dde4764037e3a440300e9
Zahra <zkeshtkarz@gmail.com>
2020-08-03 19:20:44 +0430

    set the database data in the setting file

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class User(models.Model):
+    userName = models.CharField(max_length=100)
+    email =  models.EmailField(blank=True,primary_key = True)
+    passWord =  models.CharField(max_length=20, unique=True)
+    code = models.CharField(max_length=20, unique=True)
+    
+
+
+
+
 # Create your models here.
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+ from django.http import HttpResponse
+ @require_http_methods(["POST"])
+def my_view(request):
+    
 # Create your views here.
